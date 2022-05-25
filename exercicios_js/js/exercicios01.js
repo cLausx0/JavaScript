60c3af08f9150a6bb4fe58dc1daf6116992e5d5d
cLausx0 <david.almeida2707@gmail.com>
2022-05-25 16:10:06 -0300

    Adicionando o 4º quesito

--- exercicios_js/js/exercicios01.js
+++ exercicios_js/js/exercicios01.js
@@ -38,8 +38,115 @@ function imc() {
     IMC.value = valor.toFixed(1)
 }
 
-function tabelaCarros(){
-    let html = ' ';
-    html += '<tr> <td> Teste </td></tr>'
-    document.getElementById('tableCars').innerHTML = html
+function tabelaCarros(carros) {
+    const table = document.getElementById('tableCars')
+    let idx = true
+    let newtrElement;
+    let newTDElement;
+
+    if (idx) {
+        newtrElement = document.createElement('tr')
+    
+        table.appendChild(newtrElement)
+        for(let i of carros){
+            for (let x in i) {
+                newTDElement = document.createElement('td')
+                newTDElement.innerHTML = x
+                newtrElement.appendChild(newTDElement)
+            }
+            idx = false
+            break
+        }
+    }
+
+    for (let i of carros) {
+        newtrElement = document.createElement('tr')
+
+        table.appendChild(newtrElement)
+
+        for (let x in i) {
+            newTDElement = document.createElement('td')
+            newTDElement.innerHTML = i[x]
+            newtrElement.appendChild(newTDElement)
+        }
+    }
+
+}
+
+function tabelaCarrosMaisEmplacados(carros){
+    const table = document.getElementById('tableCars')
+    let idx = true
+    carros.sort(function(a,b){
+        if (a.emplacamentos < b.emplacamentos) return 1
+        if (a.emplacamentos > b.emplacamentos) return -1
+        return 0
+    })
+
+    let newtrElement;
+    let newTDElement;
+
+    if (idx) {
+        newtrElement = document.createElement('tr')
+    
+        table.appendChild(newtrElement)
+        for(let i of carros){
+            for (let x in i) {
+                newTDElement = document.createElement('td')
+                newTDElement.innerHTML = x
+                newtrElement.appendChild(newTDElement)
+            }
+            idx = false
+            break
+        }
+    }
+
+    for (let i of carros) {
+        newtrElement = document.createElement('tr')
+
+        table.appendChild(newtrElement)
+
+        for (let x in i) {
+            newTDElement = document.createElement('td')
+            newTDElement.innerHTML = i[x]
+            newtrElement.appendChild(newTDElement)
+        }
+    }
+
+}
+
+function tabelaCarrosIpva(carros){
+    const table = document.getElementById('tableCars')
+    let idx = true
+    for(let i of carros){
+        i.ipva = (i.preco *3/100)/100
+    }
+    let newtrElement;
+    let newTDElement;
+
+    if (idx) {
+        newtrElement = document.createElement('tr')
+    
+        table.appendChild(newtrElement)
+        for(let i of carros){
+            for (let x in i) {
+                newTDElement = document.createElement('td')
+                newTDElement.innerHTML = x
+                newtrElement.appendChild(newTDElement)
+            }
+            idx = false
+            break
+        }
+    }
+
+    for (let i of carros) {
+        newtrElement = document.createElement('tr')
+
+        table.appendChild(newtrElement)
+
+        for (let x in i) {
+            newTDElement = document.createElement('td')
+            newTDElement.innerHTML = i[x]
+            newtrElement.appendChild(newTDElement)
+        }
+    }
 }
