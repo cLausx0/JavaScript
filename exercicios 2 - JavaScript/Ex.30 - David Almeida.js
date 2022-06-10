c8057446dc052f7cd0882e00063471ab481552fd
cLausx0 <david.almeida2707@gmail.com>
2022-06-10 00:06:13 -0300

    Atividade Finalizada

--- exercicios 2 - JavaScript/Ex.30 - David Almeida.js
+++ exercicios 2 - JavaScript/Ex.30 - David Almeida.js
@@ -13,12 +13,14 @@ function receberMelhorEstudante(objetoEstudantes){
             mediaMaior = media;
             nomeMaior = x;
         }
-        melhorEstudante.nome
+        melhorEstudante.nome = nomeMaior;
+        melhorEstudante.media = mediaMaior;
     }
+    return melhorEstudante
 }
 
-receberMelhorEstudante({
+console.log(receberMelhorEstudante({
     Joao: [8, 7.6, 8.9, 6], // média 7.625
     Mariana: [9, 6.6, 7.9, 8], // média 7.875
     Carla: [7, 7, 8, 9] // média 7.75
-    })
+    }))
